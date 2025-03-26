04de465ec90515f9806294622cbeb86126ba4c39
lett <letterr2022@gmail.com>
2025-03-26 21:41:22 +0800

    fix 109/110

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -3,12 +3,12 @@
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a
 // hint.
 
-
-
 fn main() {
     let mut res = 42;
-    let option = Some(12);
-    while let Some(x) = option {res += x;}
+    let mut option = Some(12);
+    while let Some(x) = option.take() {
+        res += x;
+    }
 
     println!("{}", res);
 }
